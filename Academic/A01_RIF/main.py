96fa1fb40b4b7382f05b53692b0eaa19ef07f9b5
Nan Zhou <joe.zhounan@gmail.com>
2024-09-04 13:58:59 +0100

    123

---
+++ Academic/A01_RIF/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   main.py
+@Time    :   2024/09/04 13:57:56
+@Author  :   Nan Zhou
+@Version :   0.3
+@Desc    :   None
+'''
+#############################################
